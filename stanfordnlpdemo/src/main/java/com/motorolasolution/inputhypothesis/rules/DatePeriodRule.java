822803b75fd49def27569444f3daf3460ed83e4d
Egor Gritsina <egorgrisina@gmail.com>
2016-03-31 19:29:12 +0400

    fix DatePeriodRule rule for work with confidence

--- stanfordnlpdemo/src/main/java/com/motorolasolution/inputhypothesis/rules/DatePeriodRule.java
+++ stanfordnlpdemo/src/main/java/com/motorolasolution/inputhypothesis/rules/DatePeriodRule.java
@@ -5,7 +5,9 @@ import com.motorolasolution.inputhypothesis.InputHypothesis;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import edu.stanford.nlp.trees.Tree;
 
@@ -17,12 +19,15 @@ public class DatePeriodRule extends BaseHypothesisRule {
         List<InputHypothesis> result = new ArrayList<InputHypothesis>();
         result.addAll(inputHypothesisList);
 
+        int inputListSize = result.size();
 
-
-        /*int inputListSize = result.size();
         for (int i = 0; i < inputListSize; i++) {
-            result.add(getNewTree(removeDatePeriod(result.get(i).deepCopy())));
-        }*/
+            Map<Tree, Double> resultMap = removeDatePeriod(result.get(i).getHTree().deepCopy(), result.get(i).getHConfidence());
+
+            for (Map.Entry entry : resultMap.entrySet()) {
+                result.add(new InputHypothesis(getNewTree((Tree) entry.getKey()), (Double) entry.getValue()));
+            }
+        }
 
         result = cleanHypothesisList(result);
 
@@ -35,7 +40,7 @@ public class DatePeriodRule extends BaseHypothesisRule {
         return result;
     }
 
-    private Tree removeDatePeriod(Tree tree){
+    private Map<Tree, Double> removeDatePeriod(Tree tree, double confidence){
 
         Tree[] childs = tree.children();
 
@@ -56,18 +61,26 @@ public class DatePeriodRule extends BaseHypothesisRule {
                 if (childrenTextValue.equals("s")){
                     String childrenValue = children.value().toLowerCase().replaceAll("[^0-9]","");
                     children.setValue(childrenValue);
+                    confidence = confidence; //!!!!!!!!!!!!!!!!
                 }
             }
 
         } else {
 
             for (int i = 0; i < childs.length; i++) {
+
                 Tree children = childs[i];
-                Tree new_children = removeDatePeriod(children);
-                tree.setChild(i, new_children);
+                Map<Tree, Double> resultMap = removeDatePeriod(children, confidence);
+
+                for (Map.Entry entry : resultMap.entrySet()) {
+                    confidence = (Double) entry.getValue();
+                    tree.setChild(i, (Tree) entry.getKey());
+                }
             }
         }
 
-        return tree;
+        Map<Tree, Double> resultMap = new HashMap<Tree, Double>();
+        resultMap.put(tree, confidence);
+        return resultMap;
     }
 }
