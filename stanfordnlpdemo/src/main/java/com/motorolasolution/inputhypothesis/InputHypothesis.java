3b236bbc880dca280df5fa0fdd6cbe880792d2fe
Egor Gritsina <egorgrisina@gmail.com>
2016-04-14 16:34:10 +0400

    improved confidence in Hypothesis

--- stanfordnlpdemo/src/main/java/com/motorolasolution/inputhypothesis/InputHypothesis.java
+++ stanfordnlpdemo/src/main/java/com/motorolasolution/inputhypothesis/InputHypothesis.java
@@ -5,16 +5,24 @@ import edu.stanford.nlp.trees.Tree;
 public class InputHypothesis {
 
     private Tree hTree;
-    private double hConfidence;
+    private HypothesisConfidence hConfidence;
 
     public InputHypothesis() {
         hTree = null;
-        hConfidence = 0.0;
+        hConfidence = new HypothesisConfidence();
     }
 
     public InputHypothesis(Tree tree, double confidence) {
         hTree = tree;
-        hConfidence = confidence;
+        int wCount = tree.getLeaves().size();
+        int tDeep = tree.depth();
+        hConfidence = new HypothesisConfidence(wCount, tDeep, confidence);
+    }
+
+    public InputHypothesis(Tree tree, HypothesisConfidence hypothesisConfidence) {
+        hTree = tree;
+        hConfidence = new HypothesisConfidence(hypothesisConfidence.getWordCount(),
+                hypothesisConfidence.getTreeDeep(), hypothesisConfidence.getConfidence());
     }
 
     public Tree getHTree() {
@@ -24,11 +32,53 @@ public class InputHypothesis {
         hTree = tree;
     }
 
-    public double getHConfidence() {
+    public HypothesisConfidence getHConfidence() {
         return hConfidence;
     }
-    public void setHConfidence(double confidence) {
+    public void setHConfidence(HypothesisConfidence confidence) {
         hConfidence = confidence;
+    }
+
+    public class HypothesisConfidence {
+        private int wordCount;
+        private int treeDeep;
+        private double confidence;
+
+        public HypothesisConfidence(){
+            wordCount = 0;
+            treeDeep = 0;
+            confidence = 0.0;
+        }
+
+        public HypothesisConfidence(int wCont, int tDeep, double conf){
+            wordCount = wCont;
+            treeDeep = tDeep;
+            confidence = conf;
+        }
+
+        public int getWordCount() {
+            return wordCount;
+        }
+
+        public void setWordCount(int wordCount) {
+            this.wordCount = wordCount;
+        }
+
+        public int getTreeDeep() {
+            return treeDeep;
+        }
+
+        public void setTreeDeep(int treeDeep) {
+            this.treeDeep = treeDeep;
+        }
+
+        public double getConfidence() {
+            return confidence;
+        }
+
+        public void setConfidence(double confidence) {
+            this.confidence = confidence;
+        }
     }
 
 }
