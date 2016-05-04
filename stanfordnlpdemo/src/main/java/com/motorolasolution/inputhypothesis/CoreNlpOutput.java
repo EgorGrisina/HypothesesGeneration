cf04c92acfa87ec7d54eb11e0e8855593571d2ff
Egor Gritsina <egorgrisina@gmail.com>
2016-05-04 20:30:33 +0400

    parse Hyp. to S2i request

--- stanfordnlpdemo/src/main/java/com/motorolasolution/inputhypothesis/CoreNlpOutput.java
+++ stanfordnlpdemo/src/main/java/com/motorolasolution/inputhypothesis/CoreNlpOutput.java
@@ -1,6 +1,7 @@
 package com.motorolasolution.inputhypothesis;
 
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import edu.stanford.nlp.ling.CoreLabel;
@@ -14,6 +15,7 @@ public class CoreNlpOutput {
         for (CoreLabel label : list) {
             sentence+=label.word() + " ";
         }
+        sentence = sentence.substring(0, sentence.lastIndexOf(" "));
         sentence = sentence.replace(" '", "'");
         return sentence;
     }
@@ -39,6 +41,26 @@ public class CoreNlpOutput {
             out.println("");
         }
         out.flush();
+    }
+
+    public static String getS2iQuery(List<InputHypothesis> hypothesises){
+        String query = "";
+        String confidence = "";
+
+        for(InputHypothesis hypothesis : hypothesises){
+            try {
+                String phrase = java.net.URLEncoder.encode(
+                        CoreNlpOutput.getSentenceFromTree(hypothesis.getHTree()), "ISO-8859-1");
+                query+="query="+phrase+"&";
+                confidence+="confidence="+hypothesis.getHConfidence().getConfidence()+"&";
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        confidence = confidence.substring(0, confidence.lastIndexOf("&"));
+        query+=confidence;
+
+        return query;
     }
 
 }
